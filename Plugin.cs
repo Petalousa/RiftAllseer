f794607650f1ab488bc545ecee16539c4cc86b62
Petalousa <terrapetalousa@gmail.com>
2025-02-09 14:06:32 -0500

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,82 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+using BepInEx.Configuration;
+using HarmonyLib;
+using RhythmRift.Traps;
+using RhythmRift;
+using Shared.RhythmEngine;
+using Shared.Analytics;
+using System;
+
+
+namespace RiftAllseer;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("RiftOfTheNecroDancer.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    internal static ConfigEntry<bool> shouldDisableAnalytics;
+    internal static ConfigEntry<bool> shouldLogAnalytics;
+
+    internal static ConfigEntry<bool> shouldDisableHiddenTraps;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        shouldDisableHiddenTraps = Config.Bind("Traps", "Disable Hidden Traps", true, "Prevents ? traps from spawning.");
+
+        shouldDisableAnalytics = Config.Bind("Analytics", "Disable Analytics", true, "Prevents Analytics from being sent.");
+        shouldLogAnalytics = Config.Bind("Analytics", "Log Analytics to Console", false, "Shows JSON of analytics in console.");
+
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+        Logger.LogInfo("Patched");
+    }
+
+    [HarmonyPatch(typeof(RRTrapController), "SpawnTrap")]
+    [HarmonyPrefix]
+    static bool SkipAllTrapSpawns(ref TrapSpawnData trapSpawnData) {
+        if (trapSpawnData.TrapType == RRTrapType.Mystery && shouldDisableHiddenTraps.Value)
+        {
+            Console.WriteLine("Skipping ? trap spawned");
+            return false; // Skip original method
+        }
+        return true; // Execute original method
+    }
+
+    [HarmonyPatch(typeof(RREnemyController), "SpawnEnemy", new Type[] {typeof(SpawnEnemyData), typeof(Guid), typeof(FmodTimeCapsule)})]
+    [HarmonyPrefix]
+    static bool AlertEnemySpawn() {
+        //Console.WriteLine("enemy spawned");
+        return true; // Execute original method
+    }
+
+    [HarmonyPatch(typeof(RiftAnalyticsService), "SendAnalyticsEvent")]
+    [HarmonyPrefix]
+    static bool DisableAnalytics(ref string tableName, ref string dataJsonString, bool shouldLogResponse = true){
+        if (shouldLogAnalytics.Value) {
+            Console.Write("Analytics send event triggered:");
+            Console.WriteLine(tableName);
+            Console.WriteLine(dataJsonString);
+        }
+
+        if (shouldDisableAnalytics.Value){
+            return false;
+        }
+        return true;
+    }
+
+    [HarmonyPatch(typeof(RRStageController), "UploadScoreToLeaderboardAndRefreshUi")]
+    [HarmonyPrefix]
+    static bool DisableLeaderboardScores(){
+        if (shouldDisableHiddenTraps.Value){
+            Console.WriteLine("Preventing score upload.");
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# Install BepInEx
+
+This assumes you are running Windows 10, 64-bit
+
+1. Navigate to the Rift of the Necrodancer Steam Folder:
+![alt text](image.png)
+
+2. Back up this folder somewhere safe
+![alt text](image-2.png)
+
+3. Install BepInEx Mono
+
+    a. download and unzip [BepInEx 6.0.0-be.733](https://builds.bepinex.dev/projects/bepinex_be/733/BepInEx-Unity.Mono-win-x64-6.0.0-be.733%2B995f049.zip)
+
+    ![alt text](image-3.png)
+
+    b. Copy the contents into your Rift of the NecroDancer steam directory
+    ![alt text](image-4.png)
+
+4. Install this mod by copying `RiftAllseer.dll` to the plugins directory
+![alt text](image-5.png)
+![alt text](image-1.png)
+
+5. Launch rift of the necrodancer
+
+6. You should have a console window with info, meaning the mod has been launched successfully:
+![alt text](image-6.png)
+
+# Configuration
+
+After the mod is launched for the first time, there is a file in `BepInEx1 > config > RiftAllseer.cfg`
+
+In there you can change these configs:
+
+- enable/disable game analytics (disabled by default)
+- enable/disable logging game analytics to console (disabled by default)
+- enable/disable ? traps (disabled by default)
+
+> YOU MUST RESTART THE GAME FOR CONFIGURATION CHANGES TO TAKE EFFECT! 
+
+
+# More details on installing BepInEx:
+
+- docs: https://docs.bepinex.dev/master/articles/user_guide/installation/unity_mono.html
+
+- builds: https://builds.bepinex.dev/projects/bepinex_be
